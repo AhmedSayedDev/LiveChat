d7dd114b279804f0b23d25d63fcedfcb89472780
Ahmed Sayed <ahmedsayed.s1998@gmail.com>
2020-03-06 04:10:37 +0300

    Add files via upload

---
+++ public/js/chat.js
@@ -0,0 +1,65 @@
+const socket = io()
+const $messageForm        = document.querySelector('#message-form')
+const $messageFormInput   = $messageForm.querySelector('input')
+const $messageFormButton  = $messageForm.querySelector('button')
+const $messages           = document.querySelector('#messages')
+const messageTemplate     = document.querySelector('#message-template').innerHTML
+const sidebarTemplate     = document.querySelector('#sidebar-template').innerHTML
+const { username, room }  = Qs.parse(location.search, { ignoreQueryPrefix: true })
+
+const autoscroll = () => {
+    const $newMessage = $messages.lastElementChild
+    const newMessageStyles = getComputedStyle($newMessage)
+    const newMessageMargin = parseInt(newMessageStyles.marginBottom)
+    const newMessageHeight = $newMessage.offsetHeight + newMessageMargin
+    const visibleHeight   = $messages.offsetHeight
+    const containerHeight = $messages.scrollHeight
+    const scrollOffset    = $messages.scrollTop + visibleHeight
+    if (containerHeight - newMessageHeight <= scrollOffset) {
+        $messages.scrollTop = $messages.scrollHeight
+    }
+}
+
+socket.on('msg', (message) => {
+    console.log(message)
+    const html = Mustache.render(messageTemplate, {
+        username: message.username,
+        message: message.text,
+        createdAt: moment(message.createdAt).format('h:mm a')
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
+    autoscroll()
+})
+
+
+
+socket.on('roomData', ({ room, users }) => {
+    const html = Mustache.render(sidebarTemplate, {
+        room,
+        users
+    })
+    document.querySelector('#sidebar').innerHTML = html
+})
+
+$messageForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    $messageFormButton.setAttribute('disabled', 'disabled')
+    const message = e.target.elements.message.value
+    socket.emit('sendMsg', message, (error) => {
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+        if (error) {
+            return console.log(error)
+        }
+        console.log('Message sent!')
+    })
+})
+
+
+socket.emit('join', { username, room }, (error) => {
+    if (error) {
+        alert(error)
+        location.href = '/'
+    }
+})
